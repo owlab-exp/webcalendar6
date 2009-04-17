bb43bb64372ee6d281721fea07fe4cf4333554a5
ashahab <ashahab@gmail.com>
2009-04-17 20:54:24 +0000

    Add Service tests

--- WebCalendar6/test/edu/cmu/tsp6/serviceImplementations/EventServiceImplTest.java
+++ WebCalendar6/test/edu/cmu/tsp6/serviceImplementations/EventServiceImplTest.java
@@ -25,60 +25,129 @@ import edu.cmu.tsp6.bo.User;
 import edu.cmu.tsp6.client.EventService;
 import edu.cmu.tsp6.server.dao.DatabaseConnection;
 import edu.cmu.tsp6.server.dao.EventDAO;
+import edu.cmu.tsp6.server.dao.exception.EventNotExistException;
 import static org.junit.Assert.*;
+
 /**
  * @author kaalpurush
- *
+ * 
  */
 public class EventServiceImplTest {
-	private static MySqlConnection dbUnitConnection;
+	Connection connection = DatabaseConnection.getInstance().getConnection();
+
 	/**
-	 * Test method for {@link edu.cmu.tsp6.serviceImplementations.EventServiceImpl#addEvent(edu.cmu.tsp6.bo.Event)}.
+	 * Test method for
+	 * {@link edu.cmu.tsp6.serviceImplementations.EventServiceImpl#addEvent(edu.cmu.tsp6.bo.Event)}
+	 * .
 	 */
 	@Test
 	public void testAddEvent() {
-		Connection connection = DatabaseConnection.getInstance().getConnection();
+		EventDAO ed = EventDAO.getInstance();
+		int eventId = 0;
 		try {
+
 			connection.setAutoCommit(false);
-		} catch (SQLException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		
-		BirthdayEvent birthdayEvent = new BirthdayEvent();
-		
-		User owner = new User();
-		User birthdayPerson = new User();
-		
-		owner.setUserId("newGuy33");
-		birthdayPerson.setUserId("non");
-		
-		birthdayEvent.setOwner(owner);
-		birthdayEvent.setBirthdayPerson(birthdayPerson);
-		try {
-			birthdayEvent.setDate(new SimpleDateFormat("yyyy-MM-dd").parse("1984-01-25"));
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		//add an event
-		EventService es = EventServiceImpl.getInstance();
-		es.addEvent(birthdayEvent);
-		//query it using the getEvent
-//		EventDAO ed = EventDAO.getInstance();
-//		BirthdayEvent be = ed.getEventByOwner(birthdayEvent.getOwner().getUserId());
-//		assertTrue(be.getBirthdayPerson().equals(birthdayEvent.getBirthdayPerson())); 
-//		assertTrue(be.getOwner().equals(birthdayEvent.getOwner()));
-//		assertTrue(be.getDate().equals(birthdayEvent.getDate()));
-		try {
+
+			BirthdayEvent birthdayEvent = new BirthdayEvent();
+
+			User owner = new User();
+			User birthdayPerson = new User();
+
+			owner.setUserId("coconut");
+			birthdayPerson.setUserId("abin");
+
+			birthdayEvent.setOwner(owner);
+			birthdayEvent.setBirthdayPerson(birthdayPerson);
+			birthdayEvent.setDate(new SimpleDateFormat("yyyy-MM-dd")
+					.parse("1984-01-25"));
+			// add an event
+			EventService es = EventServiceImpl.getInstance();
+			es.addEvent(birthdayEvent);
+			// query it using the getEvent
+
+			BirthdayEvent be = ed.getEventByOwner(birthdayEvent.getOwner()
+					.getUserId());
+			eventId = be.getEventId();
+			assertTrue(be.getBirthdayPerson().equals(
+					birthdayEvent.getBirthdayPerson()));
+			assertTrue(be.getOwner().equals(birthdayEvent.getOwner()));
+			assertTrue(be.getDate().equals(birthdayEvent.getDate()));
+
 			connection.rollback();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+		} catch (Throwable e) {
+			try {
+				connection.rollback();
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
 			e.printStackTrace();
+		} finally {
+			ed.deleteEvent(eventId);
 		}
 	}
-		/**
-	 * Test method for {@link edu.cmu.tsp6.serviceImplementations.EventServiceImpl#getUser(java.lang.String)}.
+
+	/**
+	 * Test method for
+	 * {@link edu.cmu.tsp6.serviceImplementations.EventServiceImpl#addEvent(edu.cmu.tsp6.bo.Event)}
+	 * .
+	 */
+	@Test(expected=IllegalStateException.class)
+	public void testAddEventDuplicate() {
+		EventDAO ed = EventDAO.getInstance();
+		int eventId = 0;
+		try {
+
+			connection.setAutoCommit(false);
+
+			BirthdayEvent birthdayEvent = new BirthdayEvent();
+
+			User owner = new User();
+			User birthdayPerson = new User();
+
+			owner.setUserId("coconut");
+			birthdayPerson.setUserId("abin");
+
+			birthdayEvent.setOwner(owner);
+			birthdayEvent.setBirthdayPerson(birthdayPerson);
+			birthdayEvent.setDate(new SimpleDateFormat("yyyy-MM-dd")
+					.parse("1984-01-25"));
+			// add an event
+			EventService es = EventServiceImpl.getInstance();
+			es.addEvent(birthdayEvent);
+			// query it using the getEvent
+
+			BirthdayEvent be = ed.getEventByOwner(birthdayEvent.getOwner()
+					.getUserId());
+			eventId = be.getEventId();
+			assertTrue(be.getBirthdayPerson().equals(
+					birthdayEvent.getBirthdayPerson()));
+			assertTrue(be.getOwner().equals(birthdayEvent.getOwner()));
+			assertTrue(be.getDate().equals(birthdayEvent.getDate()));
+			//add birthday event again
+			es.addEvent(birthdayEvent);
+			connection.rollback();
+		} catch (Throwable e) {
+			try {
+				connection.rollback();
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+				
+			}
+			e.printStackTrace();
+			if (e instanceof IllegalStateException) {
+				throw (IllegalStateException)e;
+			}
+		} finally {
+			ed.deleteEvent(eventId);
+		}
+	}
+
+	/**
+	 * Test method for
+	 * {@link edu.cmu.tsp6.serviceImplementations.EventServiceImpl#getUser(java.lang.String)}
+	 * .
 	 */
 	@Test
 	public void testGetUser() {
@@ -86,7 +155,9 @@ public class EventServiceImplTest {
 	}
 
 	/**
-	 * Test method for {@link edu.cmu.tsp6.serviceImplementations.EventServiceImpl#updateEvent(edu.cmu.tsp6.bo.Event)}.
+	 * Test method for
+	 * {@link edu.cmu.tsp6.serviceImplementations.EventServiceImpl#updateEvent(edu.cmu.tsp6.bo.Event)}
+	 * .
 	 */
 	@Test
 	public void testUpdateEvent() {
